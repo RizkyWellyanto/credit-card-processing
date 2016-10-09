c45b69ee113223b454442fad47fad23f71d7b78c
Rizky Wellyanto <wellyan2@illinois.edu>
2016-10-08 23:34:56 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by rizky on 10/8/16.
+ */
+
+// modules
+var readline = require('readline');
+
+// readline for handling stdin stdout
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function main(){
+    // check input file or stdin
+    var file = process.argv[1];
+
+    if(file){
+
+    }
+}
